800e9ab7411ff8ff193fb698a263b05e6be51301
SKPdCoder <shekharkumarpatra@yahoo.in>
2017-05-24 14:14:48 +0200

    This is a code created to play the game of guessing the number betwen user and computer.
    At the game beginning the user is instructed to think a number between 0 and 100.

--- guessTheNumber.py
+++ guessTheNumber.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 """
-Author:Skpdcoder
+Author:Shekhar Kumar Patra
 
-This is a code created to play the game of guessing the number betwen user and computer.
+This is a file created to play the game of guessing the number between player and computer.
 
 At the game beginning the user is instructed to think a number between 0 and 100.
 
@@ -17,23 +17,24 @@ At the end When the computer guesses the correct number it Displays "How many gu
 
 """
 
-import random
-import time
+import random  #imported the random module
+import time    #imported the time module
 
 
-low=0
-high=100
-count=1
+low=0          #lower range for the numbers to be guessed
+high=100      #higher range for the numbers to be guessed
+count=1       #count is set for the guesses the computer has taken
 
 
-
+# Here the user instruction is given for the player of the game 
 userNum=print("Please think a number between {} and {}!\n".format(low,high))
 time.sleep(2)
-guess=random.randrange(low,high)
+guess=random.randrange(low,high)   #the first guessis initiated using random function
 print("READY!!\n")
 time.sleep(1)
 
-
+#here a function is defined for the game which will guess the number thought by the player
+#Here the function is a recursive function which calls itself again and again
 def guessGame(count,low,high,guess):
     
     guessPrint="My number-{} guess is {}!!".format(count,guess)
@@ -61,7 +62,7 @@ def guessGame(count,low,high,guess):
         print(finalPrint)
         
             
-            
+#function call is done in the main program            
 guessGame(count,low,high,guess)            
         
         
